17191465b4741e6a66abdc86ba96dd20e8f8c573
Assad <assad.hussain@academytrainee.com>
2018-07-18 09:16:23 +0100

    FileWriter done

--- src/WorkingWithFiles/Main.java
+++ src/WorkingWithFiles/Main.java
@@ -1,8 +1,6 @@
 package WorkingWithFiles;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
@@ -11,8 +9,39 @@ public class Main {
         for (int i = 0; i < 5 ; i++) {
             pList.add(new Person("Name"+i,"Occupation"+i,(int)(Math.random()*100)));
         }
-        File newFile = new File();
-        BufferedWriter bw = new BufferedWriter(new FileWriter());
+        for (Person p :pList
+             ) {
+
+            BufferedWriter bw;
+            File newFile = new File("C:\\users\\Person\\" +p.name+".txt");
+            try {
+                bw = new BufferedWriter(new FileWriter(newFile));
+                bw.write("Name:"+p.name+"\r\nOccupation:"+p.occupation+"\r\nAge:"+p.age);
+                bw.flush();
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            finally {
+
+            }
+
+        }
+        ArrayList<Person> readList = new ArrayList<>();
+        BufferedReader br;
+        try {
+            File[] fileList = new File("C:\\users\\person").listFiles();
+            for(File f: fileList) {
+                br = new BufferedReader(new FileReader(f));
+                System.out.println(f.getName());
+                while(br.ready())
+                    System.out.println(br.readLine());
+            }
+        }catch (Exception e){
+
+        }
     }
 
 }
